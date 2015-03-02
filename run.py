39f8a1661334433dd5b1fce3c4e02eb55b2dcbff
argunner <gunnerar7@gmail.com>
2015-03-02 12:58:06 +0530

    another

---
+++ run.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+#@app.route('/home')
+#@app.route('/index')
+
+def home():
+	return render_template('home.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
